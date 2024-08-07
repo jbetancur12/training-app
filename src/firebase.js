dc865055240b8c0a0598a449ec778595b4bf1495
Jorge Betancur <jorgebetancur@Jorges-MacBook-Air-2.local>
2024-08-07 17:01:52 -0500

    env

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 
 // Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAeSQYxCsRDQun726tLPDsoG5X2Od6U9ZQ",
-  authDomain: "training-app-318af.firebaseapp.com",
-  projectId: "training-app-318af",
-  storageBucket: "training-app-318af.appspot.com",
-  messagingSenderId: "789541304508",
-  appId: "1:789541304508:web:6625034a63e3a11417f8b9",
-  measurementId: "G-N8L2YX2181"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
